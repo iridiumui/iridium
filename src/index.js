653b7fd692e24f5029393a9cb874f11c4e6ef81b
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-27 08:36:29 +0800

    Fixed - Add FixedBottom helper

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Fixed from './helpers/Fixed.vue'
 import FixedTop from './helpers/FixedTop.vue'
 import FixedRight from './helpers/FixedRight.vue'
+import FixedBottom from './helpers/FixedBottom.vue'
 
 export default {
     install(Vue) {
         Vue.component('iridium-fixed', Fixed)
         Vue.component('iridium-fixed-top', FixedTop)
         Vue.component('iridium-fixed-right', FixedRight)
+        Vue.component('iridium-fixed-bottom', FixedBottom)
     }
 }
 
@@ -15,4 +17,5 @@ export {
     Fixed,
     FixedTop,
     FixedRight,
+    FixedBottom,
 }
