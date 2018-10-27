4e9da9ff7e7407a99ee0ea39c4bc5c74dbe37b06
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-27 08:36:02 +0800

    Fixed - Add FixedTop helper

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Fixed from './helpers/Fixed.vue'
+import FixedTop from './helpers/FixedTop.vue'
 
 export default {
     install(Vue) {
         Vue.component('iridium-fixed', Fixed)
+        Vue.component('iridium-fixed-top', FixedTop)
     }
 }
 
 export {
     Fixed
+    Fixed,
+    FixedTop,
 }
