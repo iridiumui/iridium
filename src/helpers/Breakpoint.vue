26b59557675b421a6a41adc827fa13d104e191fd
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-27 17:05:52 +0800

    Breakpoint - It only renders components if the media query matches

--- src/helpers/Breakpoint.vue
+++ src/helpers/Breakpoint.vue
@@ -1,6 +1,20 @@
 <script>
     export default {
+        props: {
+            mediaQuery: {
+                type: String,
+                required: true
+            }
+        },
+
+        computed: {
+            matches() {
+                return window.matchMedia(this.mediaQuery).matches
+            }
+        },
+
         render() {
+            if (!this.matches) return null
             return this.$slots.default[0]
         }
     }
--- tests/unit/helpers/breakpoint.spec.js
+++ tests/unit/helpers/breakpoint.spec.js
@@ -1,22 +1,51 @@
 import { mount } from '@vue/test-utils'
 import Breakpoint from '../../../src/helpers/Breakpoint.vue'
+import matchMediaMock from 'match-media-mock'
+
+window.matchMedia = matchMediaMock.create()
+window.resizeTo = (width, height) => {
+    window.matchMedia.setConfig({
+        type: 'screen',
+        width: width
+    })
+}
 
 describe('Breakpoint', () => {
 
-    let wrapper
-
     beforeEach(() => {
-        wrapper = mount(Breakpoint, {
+        window.resizeTo(1000, 1000)
+    })
+
+    test('it renders the default slot', () => {
+        const wrapper = mount(Breakpoint, {
+            propsData: {
+                mediaQuery: '(min-width: 400px)'
+            },
+            slots: {
+                default: '<div>default slot</div>'
+            }
+        })
+
+        expect(wrapper.html()).toBe('<div>default slot</div>')
+    })
+
+    test('it only renders if it matches the media query', () => {
+        window.resizeTo(399, 1000)
+
+        let wrapper = mount(Breakpoint, {
+            propsData: {
+                mediaQuery: '(min-width: 400px)'
+            },
             slots: {
                 default: '<div>component</div>'
             }
         })
+
+        expect(wrapper.html()).toBe(undefined)
     })
 
-    test('it renders the default slot', () => {
-        wrapper = mount(Breakpoint, {
             slots: {
-                default: '<div>default slot</div>'
+                default: '<div>component</div>'
             }
         })
 
