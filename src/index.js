db268df7a4451c8cece4dc03724fa128e43b7a3b
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-27 08:36:17 +0800

    Fixed - Add FixedRight helper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Fixed from './helpers/Fixed.vue'
 import FixedTop from './helpers/FixedTop.vue'
+import FixedRight from './helpers/FixedRight.vue'
 
 export default {
     install(Vue) {
         Vue.component('iridium-fixed', Fixed)
         Vue.component('iridium-fixed-top', FixedTop)
+        Vue.component('iridium-fixed-right', FixedRight)
     }
 }
 
@@ -12,4 +14,5 @@ export {
     Fixed
     Fixed,
     FixedTop,
+    FixedRight,
 }
