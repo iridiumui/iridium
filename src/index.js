1ab275439d436ecda258aeaa884f2b0ec4ac4ff0
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-27 12:07:28 +0800

    Plugin - The component prefix can be overwritte

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const isUndefined = require('lodash/isUndefined')
+
 import Fixed from './helpers/Fixed.vue'
 import FixedTop from './helpers/FixedTop.vue'
 import FixedRight from './helpers/FixedRight.vue'
@@ -5,12 +7,14 @@ import FixedBottom from './helpers/FixedBottom.vue'
 import FixedLeft from './helpers/FixedLeft.vue'
 
 export default {
-    install(Vue) {
-        Vue.component('iridium-fixed', Fixed)
-        Vue.component('iridium-fixed-top', FixedTop)
-        Vue.component('iridium-fixed-right', FixedRight)
-        Vue.component('iridium-fixed-bottom', FixedBottom)
-        Vue.component('iridium-fixed-left', FixedLeft)
+    install(Vue, options) {
+        const prefix = isUndefined(options.prefix) ? 'iridium-' : options.prefix
+
+        Vue.component(`${prefix}fixed`, Fixed)
+        Vue.component(`${prefix}fixed-top`, FixedTop)
+        Vue.component(`${prefix}fixed-right`, FixedRight)
+        Vue.component(`${prefix}fixed-bottom`, FixedBottom)
+        Vue.component(`${prefix}fixed-left`, FixedLeft)
     }
 }
 
