6a22322c5866779ea9447844d1febc6194fa5fbe
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-28 10:53:03 +0800

    Screen - It can render in both directions

--- src/helpers/ScreenSM.vue
+++ src/helpers/ScreenSM.vue
@@ -1,13 +1,28 @@
 <template>
-    <breakpoint>
+    <breakpoint :media-query="query">
         <slot></slot>
     </breakpoint>
 </template>
 
 <script>
-    import Breakpoint from './Breakpoint'
+    import Breakpoint from './Breakpoint.vue'
 
     export default {
         components: {
             Breakpoint
         },
+
+        props: {
+            direction: {
+                type: String,
+                default: 'up'
+            }
+        },
+
+        computed: {
+            query() {
+                return this.direction === 'up' ? '(min-width: 576px)' : '(max-width: 767.98px)'
+            }
+        }
+    }
+</script>
