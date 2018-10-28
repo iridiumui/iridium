d43f9dff657a3e1fb22cfc43af58c4610ef009f3
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-28 10:55:18 +0800

    Screen - Make XS helper

---
+++ src/helpers/ScreenXS.vue
@@ -0,0 +1,28 @@
+<template>
+    <breakpoint :media-query="query">
+        <slot></slot>
+    </breakpoint>
+</template>
+
+<script>
+    import Breakpoint from './Breakpoint.vue'
+
+    export default {
+        components: {
+            Breakpoint
+        },
+
+        props: {
+            direction: {
+                type: String,
+                default: 'down'
+            }
+        },
+
+        computed: {
+            query() {
+                return this.direction === 'up' ? '(min-width: 0px)' : '(max-width: 575.98px)'
+            }
+        }
+    }
+</script>
