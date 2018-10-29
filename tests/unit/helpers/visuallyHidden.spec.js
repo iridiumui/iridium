e66257d4c1d23edb54b50e93ce6adfb5c9781e13
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-29 09:58:34 +0800

    VisuallyHidden - It hides the element visually

--- tests/unit/helpers/visuallyHidden.spec.js
+++ tests/unit/helpers/visuallyHidden.spec.js
@@ -6,11 +6,22 @@ describe('VisuallyHidden', () => {
     test('it renders the default slot', () => {
         const wrapper = mount(VisuallyHidden, {
             slots: {
+                default: '<div>default slot</div>'
+            }
+        })
+
+        expect(wrapper.is('div')).toBe(true)
+        expect(wrapper.text()).toBe('default slot')
+    })
+
+    test('it hides the component for visual users', () => {
+        const wrapper = mount(VisuallyHidden, {
+            slots: {
                 default: '<div>component</div>'
             }
         })
 
-        expect(wrapper.html()).toBe('<div>component</div>')
+        expect(wrapper.attributes('style')).toBe('border:0;clip:rect(0 0 0 0);height:1px;width:1px;margin:-1px;padding:0;overflow:hidden;position:absolute;')
     })
 
 })
