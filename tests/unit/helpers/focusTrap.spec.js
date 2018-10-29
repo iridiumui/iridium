021250e1a82495c4fc55c7e405f3deb3ac51c193
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-10-29 13:04:56 +0800

    FocusTrap - It can be toggled on and off

--- tests/unit/helpers/focusTrap.spec.js
+++ tests/unit/helpers/focusTrap.spec.js
@@ -13,4 +13,36 @@ describe('FocusTrap', () => {
         expect(wrapper.html()).toBe('<div>default slot</div>')
     })
 
+    test('it can be activated and deactivated', () => {
+        const wrapper = mount(FocusTrap, {
+            propsData: {
+                active: false
+            },
+            slots: {
+                default: `
+                    <div>
+                        <button>focusable</button>
+                        <input type="text">
+                    </div>
+                `
+            }
+        })
+
+        wrapper.vm.focusTrap.activate = jest.fn(x => x)
+        wrapper.vm.focusTrap.deactivate = jest.fn(x => x)
+
+        wrapper.setProps({
+            active: true
+        })
+
+        expect(wrapper.vm.focusTrap.activate.mock.calls.length).toBe(1)
+
+        wrapper.setProps({
+            active: false
+        })
+
+        expect(wrapper.vm.focusTrap.deactivate.mock.calls.length).toBe(1)
+
+    })
+
 })
