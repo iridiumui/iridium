f96df58a074e64d4e48818a74b0f26ca745bef32
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-01 17:12:43 +0800

    TransitionBase - It can accept a duration and timing function prop

--- src/transitions/TransitionBase.vue
+++ src/transitions/TransitionBase.vue
@@ -1,5 +1,5 @@
 <template>
-    <transition :name="name">
+    <transition :name="name" v-on="hooks">
         <slot></slot>
     </transition>
 </template>
@@ -11,7 +11,39 @@
                 type: String,
                 required: true
             },
+            duration: {
+                type: Number,
+                default: 200
+            },
+            timingFunction: {
+                type: String,
+                default: 'ease'
+            }
         },
 
+        computed: {
+            hooks() {
+                return {
+                    beforeEnter: this.setDuration,
+                    afterEnter: this.cleanUpDuration,
+                    beforeLeave: this.setDuration,
+                    afterLeave: this.cleanUpDuration,
+                    ...this.$listeners
+                };
+            }
+        },
+
+        methods: {
+            setDuration(el) {
+                console.log('setting up')
+                el.style.transition = 'all'
+                el.style.transitionDuration = `${this.duration}ms`
+                el.style.transitionTimingFunction = this.timingFunction
+            },
+            cleanUpDuration(el) {
+                el.style.transitionDuration = ''
+                el.style.transitionTimingFunction = ''
+            }
+        }
     }
 </script>
