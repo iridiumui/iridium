a2a097764d165f6dc3b1efb2c5a2d114605fcca2
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-02 15:23:41 +0800

    InView - It can render both a default slot and a scoped slot

--- src/helpers/InView.vue
+++ src/helpers/InView.vue
@@ -1,8 +1,9 @@
 <script>
-
     export default {
         render() {
             return this.$slots.default[0]
+            return this.$scopedSlots.hasOwnProperty('default') ? this.$scopedSlots.default({
+            }) : this.$slots.default[0]
         }
     }
 </script>
--- tests/unit/helpers/inView.spec.js
+++ tests/unit/helpers/inView.spec.js
@@ -1,11 +1,25 @@
 import { mount } from '@vue/test-utils'
 import InView from '../../../src/helpers/InView.vue'
 
+window.IntersectionObserver = jest.fn(function() {
+    this.observe = jest.fn()
+})
 describe('InView', () => {
 
-    test('it renders the default slot', () => {
+    test('it can render the default slot', () => {
         const wrapper = mount(InView, {
             slots: {
+                default: '<div>default slot</div>'
+            }
+        })
+
+        expect(wrapper.is('div')).toBe(true)
+        expect(wrapper.text()).toBe('default slot')
+    })
+
+    test('it can render a scoped slot', () => {
+        const wrapper = mount(InView, {
+            scopedSlots: {
                 default: '<div>default slot</div>'
             }
         })
