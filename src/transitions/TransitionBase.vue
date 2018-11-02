233b6187da9cc342b08cb3d010808ec1fc1c98db
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-02 11:59:13 +0800

    TransitionBase - Support transition origin

--- src/transitions/TransitionBase.vue
+++ src/transitions/TransitionBase.vue
@@ -18,6 +18,10 @@
             easing: {
                 type: String,
                 default: 'ease'
+            },
+            origin: {
+                type: String,
+                default: '50% 50%'
             }
         },
 
@@ -37,10 +41,12 @@
             setUpTransition(el) {
                 el.style.transitionDuration = `${this.duration}ms`
                 el.style.transitionTimingFunction = this.easing
+                el.style.transitionOrigin = this.origin
             },
             cleanUpTransition(el) {
                 el.style.transitionDuration = ''
                 el.style.transitionTimingFunction = ''
+                el.style.transitionOrigin = 'this.origin'
             }
         }
     }
