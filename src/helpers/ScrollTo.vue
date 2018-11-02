e72314c045e7019ff101b818ed283a0e16724c89
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-02 20:06:08 +0800

    ScrollTo - It scrolls when the button is clicked

--- src/helpers/ScrollTo.vue
+++ src/helpers/ScrollTo.vue
@@ -1,8 +1,17 @@
 <script>
     export default {
+        methods: {
+            scroll() {
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                })
+            },
+        },
 
         render() {
             return this.$scopedSlots.default({
+                scroll: this.scroll
             })
         }
     }
--- tests/unit/helpers/scrollTo.spec.js
+++ tests/unit/helpers/scrollTo.spec.js
@@ -3,6 +3,10 @@ import ScrollTo from '../../../src/helpers/ScrollTo.vue'
 
 describe('ScrollTo', () => {
 
+    beforeEach(() => {
+        window.scrollTo = jest.fn()
+    })
+
     test('it renders the default scoped slot', () => {
         const wrapper = mount(ScrollTo, {
             scopedSlots: {
@@ -12,6 +16,18 @@ describe('ScrollTo', () => {
 
         expect(wrapper.is('div')).toBe(true)
         expect(wrapper.text()).toBe('default slot')
+    })
+
+    test('it scrolls when the button is clicked', () => {
+        const wrapper = mount(ScrollTo, {
+            scopedSlots: {
+                default: '<button @click="props.scroll">Scroll</button>'
+            }
+        })
+
+        wrapper.trigger('click')
+
+        expect(window.scrollTo).toHaveBeenCalled()
     })
 
 })
