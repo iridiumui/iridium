6d0a5c944840d89866685ca40ed1c113cb06a0ba
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-02 15:56:29 +0800

    InView - It can target a custom element or the slot element

--- src/helpers/InView.vue
+++ src/helpers/InView.vue
@@ -1,8 +1,58 @@
 <script>
     export default {
+        props: {
+            element: {
+                type: String
+            },
+        },
+
+        data() {
+            return {
+                inView: false,
+                observer: null
+            }
+        },
+
+        mounted() {
+            this.setupObserver()
+        },
+
+        beforeDestroy() {
+            this.destroyObserver()
+        },
+
+        computed: {
+            target() {
+                return document.querySelector(this.element) || this.$el
+            },
+        },
+
+        methods: {
+            setupObserver() {
+                this.destroyObserver()
+                this.observer = new IntersectionObserver((entries, observer) => {
+                }, {
+                })
+
+                this.observer.observe(this.target)
+            },
+
+            destroyObserver() {
+                if (!this.observer) return
+                this.observer.disconnect()
+                this.observer = null
+            }
+        },
+
+        watch: {
+            $route(to, from) {
+                this.setupObserver()
+            }
+        },
+
         render() {
-            return this.$slots.default[0]
             return this.$scopedSlots.hasOwnProperty('default') ? this.$scopedSlots.default({
+                inView: this.inView
             }) : this.$slots.default[0]
         }
     }
--- tests/unit/helpers/inView.spec.js
+++ tests/unit/helpers/inView.spec.js
@@ -1,10 +1,18 @@
 import { mount } from '@vue/test-utils'
 import InView from '../../../src/helpers/InView.vue'
 
-window.IntersectionObserver = jest.fn(function() {
-    this.observe = jest.fn()
-})
 describe('InView', () => {
+
+    let observe
+
+    beforeEach(() => {
+        document.body.innerHTML = ''
+
+        observe = jest.fn(x => x)
+        window.IntersectionObserver = jest.fn(function() {
+            this.observe = observe
+        })
+    })
 
     test('it can render the default slot', () => {
         const wrapper = mount(InView, {
@@ -28,4 +36,32 @@ describe('InView', () => {
         expect(wrapper.text()).toBe('default slot')
     })
 
+    test('it can target any element on the page', () => {
+        document.body.innerHTML = `
+            <div>
+                <div class="some-element">some element</div>
+            </div>
+        `
+
+        const wrapper = mount(InView, {
+            slots: {
+                default: '<div class="default">default slot</div>'
+            },
+            propsData: {
+                element: '.some-element'
+            }
+        })
+
+        expect(observe.mock.calls[0][0].classList.contains('some-element')).toBe(true)
+    })
+
+    test('it can target the slot element', () => {
+        const wrapper = mount(InView, {
+            slots: {
+                default: '<div class="default">default slot</div>'
+            }
+        })
+
+        expect(observe.mock.calls[0][0].classList.contains('default')).toBe(true)
+    })
 })
