375a29d92860669e5430d30c206f6da59aee5792
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-11-05 10:09:56 +0800

    ClickOutside - It emits an event if another element is clicked

--- src/helpers/ClickOutside.vue
+++ src/helpers/ClickOutside.vue
@@ -1,6 +1,12 @@
 <script>
     export default {
 
+        mounted() {
+            document.addEventListener('click', (e) => {
+                    this.$emit('clickoutside')
+            })
+        },
+
         render() {
             return this.$slots.default[0]
         }
--- tests/unit/helpers/clickOutside.spec.js
+++ tests/unit/helpers/clickOutside.spec.js
@@ -3,6 +3,10 @@ import ClickOutside from '../../../src/helpers/ClickOutside.vue'
 
 describe('ClickOutside', () => {
 
+    beforeEach(() => {
+        document.body.innerHTML = ''
+    })
+
     test('it renders the default slot', () => {
         const wrapper = mount(ClickOutside, {
             slots: {
@@ -12,6 +16,23 @@ describe('ClickOutside', () => {
 
         expect(wrapper.is('div')).toBe(true)
         expect(wrapper.text()).toBe('default slot')
+    })
+
+    test('it will emit an event when another element is clicked', () => {
+        document.body.innerHTML = `
+            </div>
+                <button class="other-element">Button</button>
+            </div>
+        `
+        const wrapper = mount(ClickOutside, {
+            slots: {
+                default: '<div>default slot</div>'
+            }
+        })
+
+        document.querySelector('.other-element').click()
+
+        expect(wrapper.emitted().clickoutside).toBeTruthy()
     })
 
 })
