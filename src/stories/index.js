61c36892758b661c9d73cc763a494e8490d4876a
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-01-21 21:31:56 +0100

    Breakpoint Helpers stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,4 +1,5 @@
 import './helpers/Breakpoint.stories'
+import './helpers/BreakpointHelpers.stories'
 import './helpers/ClickOutside.stories'
 import './helpers/Fixed.stories'
 import './helpers/FixedHelpers.stories'
