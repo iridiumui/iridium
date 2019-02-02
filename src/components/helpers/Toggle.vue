ac28eb91e10717d6cf66ed756ce82cbbe5afef7e
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-02-02 14:41:05 +0100

    Toggle - The slots know whether it's open

--- src/components/helpers/Toggle.vue
+++ src/components/helpers/Toggle.vue
@@ -1,12 +1,22 @@
 <script>
     export default {
+        data() {
+            return {
+                open: false
+            };
+        },
+
         render(createElement) {
+            const props = {
+                open: this.open,
+            };
+
             /* eslint-disable */
             return this.$scopedSlots.hasOwnProperty("default") ?
-                this.$scopedSlots.default() :
+                this.$scopedSlots.default(props) :
                 createElement("div", [
-                    this.$scopedSlots.button(),
-                    this.$scopedSlots.content()
+                    this.$scopedSlots.button(props),
+                    this.$scopedSlots.content(props)
                 ]);
             /* eslint-enable */
         }
--- src/stories/helpers/Toggle.stories.js
+++ src/stories/helpers/Toggle.stories.js
@@ -41,4 +41,64 @@ export default storiesOf("Helpers|Toggle", module)
                 </div>
             </Toggle>
         `
+    }))
+    .add("The slots know whether it's open", () => ({
+        components: { Toggle },
+        template: `
+            <main>
+                <h2>Named scoped slots:</h2>
+                <Toggle>
+                    <button
+                        type="button"
+                        slot="button"
+                        slot-scope="{ open }"
+                    >
+                        Button (Open state: {{ open }})
+                    </button>
+                    <div
+                        slot="content"
+                        slot-scope="{ open }"
+                    >
+                        <h2>Content</h2>
+                        <p>
+                            Open state: {{ open }}
+                        </p>
+                        <p>
+                            <i>
+                                (This story doesn't actually hide content when it's closed)
+                            </i>
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
+                        </p>
+                    </div>
+                </Toggle>
+
+                <h2>Default scoped slot:</h2>
+                <Toggle>
+                    <div
+                        slot-scope="{ open }"
+                    >
+                        <button type="button">
+                            Button (Open state: {{ open }})
+                        </button>
+                        <div>
+                            <h2>Content</h2>
+                            <p>
+                                Open state: {{ open }}
+                            </p>
+                            <p>
+                                <i>
+                                    (This story doesn't actually hide content when it's closed)
+                                </i>
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
+                            </p>
+                        </div>
+                    </div>
+                </Toggle>
+            </main>
+        `
+    }))
     }));
