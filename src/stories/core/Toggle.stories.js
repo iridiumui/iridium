5a4fe65b72daac787b65237c36863f19fefbead0
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-02-27 18:02:26 +0100

    Toggle - Add Tailwind styles

--- src/stories/core/Toggle.stories.js
+++ src/stories/core/Toggle.stories.js
@@ -1,15 +1,18 @@
 import { storiesOf } from "@storybook/vue";
+import centered from "@storybook/addon-centered";
 import Toggle from "@/components/core/Toggle";
 
-export default storiesOf("Helpers|Toggle", module)
+export default storiesOf("Core|Toggle", module)
+    .addDecorator(centered)
     .add("It can render a button and some content", () => ({
         components: { Toggle },
         template: `
-            <Toggle>
+            <Toggle class="max-w-2/3 m-auto">
                 <button
                     type="button"
                     slot="button"
                     slot-scope="props"
+                    class="button mb-4"
                 >
                     Button
                 </button>
@@ -25,11 +28,14 @@ export default storiesOf("Helpers|Toggle", module)
     .add("It can render a default scoped slot", () => ({
         components: { Toggle },
         template: `
-            <Toggle>
+            <Toggle class="max-w-2/3 m-auto">
                 <div
                     slot-scope="props"
                 >
-                    <button type="button">
+                    <button
+                        type="button"
+                        class="button mb-4"
+                    >
                         Button
                     </button>
                     <div>
@@ -45,13 +51,14 @@ export default storiesOf("Helpers|Toggle", module)
     .add("The slots know whether it's open", () => ({
         components: { Toggle },
         template: `
-            <main>
-                <h2>Named scoped slots:</h2>
-                <Toggle>
+            <main class="max-w-2/3 m-auto">
+                <h2 class="mt-8 mb-4">Named scoped slots:</h2>
+                <Toggle class="mb-12">
                     <button
                         type="button"
                         slot="button"
                         slot-scope="{ open }"
+                        class="button mb-4"
                     >
                         Button (Open state: {{ open }})
                     </button>
@@ -59,11 +66,11 @@ export default storiesOf("Helpers|Toggle", module)
                         slot="content"
                         slot-scope="{ open }"
                     >
-                        <h2>Content</h2>
-                        <p>
+                        <h2 class="mb-2">Content</h2>
+                        <p class="mb-2">
                             Open state: {{ open }}
                         </p>
-                        <p>
+                        <p class="mb-2">
                             <i>
                                 (This story doesn't actually hide content when it's closed)
                             </i>
@@ -74,20 +81,23 @@ export default storiesOf("Helpers|Toggle", module)
                     </div>
                 </Toggle>
 
-                <h2>Default scoped slot:</h2>
+                <h2 class="mb-4">Default scoped slot:</h2>
                 <Toggle>
                     <div
                         slot-scope="{ open }"
                     >
-                        <button type="button">
+                        <button
+                            type="button"
+                            class="button mb-4"
+                        >
                             Button (Open state: {{ open }})
                         </button>
                         <div>
-                            <h2>Content</h2>
-                            <p>
+                            <h2 class="mb-2">Content</h2>
+                            <p class="mb-2">
                                 Open state: {{ open }}
                             </p>
-                            <p>
+                            <p class="mb-2">
                                 <i>
                                     (This story doesn't actually hide content when it's closed)
                                 </i>
@@ -104,13 +114,14 @@ export default storiesOf("Helpers|Toggle", module)
     .add("It can be open by default", () => ({
         components: { Toggle },
         template: `
-            <main>
-                <h2>Named scoped slots:</h2>
-                <Toggle :open-default="true">
+            <main class="max-w-2/3 m-auto">
+                <h2 class="mt-8 mb-4">Named scoped slots:</h2>
+                <Toggle :open-default="true" class="mb-8">
                     <button
                         type="button"
                         slot="button"
                         slot-scope="{ open }"
+                        class="button mb-4"
                     >
                         Button (Open state: {{ open }})
                     </button>
@@ -118,28 +129,31 @@ export default storiesOf("Helpers|Toggle", module)
                         slot="content"
                         slot-scope="{ open }"
                     >
-                        <h2>Content</h2>
-                        <p>
+                        <h2 class="mb-2">Content</h2>
+                        <p class="mb-2">
                             Open state: {{ open }}
                         </p>
-                        <p>
+                        <p class="mb-2">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
                         </p>
                     </div>
                 </Toggle>
 
-                <h2>Default scoped slot:</h2>
+                <h2 class="mb-4">Default scoped slot:</h2>
                 <Toggle :open-default="true">
                     <div slot-scope="{ open }">
-                        <button type="button">
+                        <button
+                            type="button"
+                            class="button mb-4"
+                        >
                             Button (Open state: {{ open }})
                         </button>
                         <div>
-                            <h2>Content</h2>
-                            <p>
+                            <h2 class="mb-2">Content</h2>
+                            <p class="mb-2">
                                 Open state: {{ open }}
                             </p>
-                            <p>
+                            <p class="mb-2">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
                             </p>
                         </div>
@@ -151,15 +165,16 @@ export default storiesOf("Helpers|Toggle", module)
     .add("The slots can toggle", () => ({
         components: { Toggle },
         template: `
-            <main>
-                <h2>Named scoped slots:</h2>
-                <Toggle>
+            <main class="w-2/3 m-auto">
+                <h2 class="mb-4">Named scoped slots:</h2>
+                <Toggle class="mb-8">
                     <button
                         type="button"
                         slot="button"
                         slot-scope="{ open, toggle }"
-                        :disabled="open"
                         @click="toggle"
+                        class="button mb-4"
+                        :disabled="open"
                     >
                         Open Content
                     </button>
@@ -168,36 +183,40 @@ export default storiesOf("Helpers|Toggle", module)
                         slot-scope="{ open, toggle }"
                         v-show="open"
                     >
-                        <h2>Content</h2>
-                        <p>
+                        <h2 class="mb-2">Content</h2>
+                        <p class="mb-2">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
                         </p>
                         <button
                             type="button"
                             @click="toggle"
+                            class="button"
                         >
                             Close Content
                         </button>
                     </div>
                 </Toggle>
-
-                <h2>Default scoped slot:</h2>
+                <hr class="border border-1" />
+                <h2 class="mb-4">Default scoped slot:</h2>
                 <Toggle>
                     <div slot-scope="{ open, toggle }">
                         <button
                             type="button"
                             @click="toggle"
+                            class="button mb-4"
+                            :disabled="open"
                         >
                             Open Content
                         </button>
                         <div v-show="open">
-                            <h2>Content</h2>
-                            <p>
+                            <h2 class="mb-2">Content</h2>
+                            <p class="mb-2">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, dicta similique dolorum itaque temporibus placeat doloremque sunt repellat molestiae sed repudiandae laudantium sapiente veniam accusamus. Quas repellat sapiente impedit tenetur!
                             </p>
                             <button
                                 type="button"
                                 @click="toggle"
+                                class="button"
                             >
                                 Close Content
                             </button>
