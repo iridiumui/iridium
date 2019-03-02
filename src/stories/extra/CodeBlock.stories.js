f3047c22e8ec041d5cc196479e641f120eb7b315
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-03-02 18:27:00 +0100

    Update CodeBlock

--- src/stories/extra/CodeBlock.stories.js
+++ src/stories/extra/CodeBlock.stories.js
@@ -9,23 +9,25 @@ export default storiesOf("Extra|Code Block", module)
         template: `
             <div>
                 <CodeBlock class="bg-grey-lighter w-80 relative">
-                    <pre slot="content" class="inline-block w-80">
-                        <code class="block w-80">
+                    <template #content>
+                        <pre class="inline-block w-80">
+                            <code class="block w-80">
 .some-css-class {
     display: flex;
     justify-content: center;
     align-items: center;
 }
-                        </code>
-                    </pre>
-                    <button
-                        type="button"
-                        slot="button"
-                        slot-scope="{ copy }"
-                        class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
-                    >
-                        Copy Button (not hooked up)
-                    </button>
+                            </code>
+                        </pre>
+                    </template>
+                    <template #button="{ copy }">
+                        <button
+                            type="button"
+                            class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
+                        >
+                            Copy Button (not hooked up)
+                        </button>
+                    </template>
                 </CodeBlock>
             </div>
         `
@@ -35,24 +37,26 @@ export default storiesOf("Extra|Code Block", module)
         template: `
             <div>
                 <CodeBlock class="bg-grey-lighter w-80 relative">
-                    <pre slot="content" class="inline-block w-80">
-                        <code class="block w-80">
+                    <template #content>
+                        <pre slot="content" class="inline-block w-80">
+                            <code class="block w-80">
 .some-css-class {
     display: flex;
     justify-content: center;
     align-items: center;
 }
-                        </code>
-                    </pre>
-                    <button
-                        type="button"
-                        slot="button"
-                        slot-scope="{ copy }"
-                        @click="copy"
-                        class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
-                    >
-                        Copy
-                    </button>
+                            </code>
+                        </pre>
+                    </template>
+                    <template #button="{ copy }">
+                        <button
+                            type="button"
+                            @click="copy"
+                            class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
+                        >
+                            Copy
+                        </button>
+                    </template>
                 </CodeBlock>
                 <textarea
                     placeholder="Past the code here"
@@ -71,24 +75,26 @@ export default storiesOf("Extra|Code Block", module)
         template: `
             <div>
                 <CodeBlock :success-callback="success" class="bg-grey-lighter w-80 relative">
-                    <pre slot="content" class="inline-block w-80">
-                        <code class="block w-80">
+                    <template #content>
+                        <pre slot="content" class="inline-block w-80">
+                            <code class="block w-80">
 .some-css-class {
     display: flex;
     justify-content: center;
     align-items: center;
 }
-                        </code>
-                    </pre>
-                    <button
-                        type="button"
-                        slot="button"
-                        slot-scope="{ copy }"
-                        @click="copy"
-                        class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
-                    >
-                        Copy
-                    </button>
+                            </code>
+                        </pre>
+                    </template>
+                    <template #button="{ copy }">
+                        <button
+                            type="button"
+                            @click="copy"
+                            class="absolute pin-t pin-r text-sm bg-pink p-1 text-white font-bold uppercase"
+                        >
+                            Copy
+                        </button>
+                    </template>
                 </CodeBlock>
                 <textarea
                     placeholder="Past the code here"
